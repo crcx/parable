deedfd33b3f84c964467777372e249d1bd2a76af
crc <crc@questor>
2013-08-30 17:36:15 +0000

    begin work on using console module to add colors; start of legend style stack display

--- pythonista/parable-pythonista.py
+++ pythonista/parable-pythonista.py
@@ -1364,25 +1364,35 @@ def dump_stack():
     global stack
     i = 0
     while i < len(stack):
-        sys.stdout.write("\t" + str(i))
+        console.set_color(0, 0, 0)
+        print "\t" + str(i),
         if types[i] == TYPE_NUMBER:
-            sys.stdout.write("\t#" + str(stack[i]))
+            console.set_color(141/255, 182/255, 205/255)
+            print "\t#" + str(stack[i])
         elif types[i] == TYPE_CHARACTER:
-            sys.stdout.write("\t$" + str(chr(stack[i])))
+            console.set_color(139/255, 168/255, 112/255)
+            print "\t$" + str(chr(stack[i]))
         elif types[i] == TYPE_STRING:
-            sys.stdout.write("\t'" + slice_to_string(stack[i]) +"'")
+            console.set_color(140/255, 120/255, 83/255)
+            print "\t'" + slice_to_string(stack[i]) +"'"
+            console.set_color(0, 0, 0)
+            print "\t\tstored at: " + str(stack[i])
         elif types[i] == TYPE_FUNCTION:
-            sys.stdout.write("\t&" + str(stack[i]))
+            console.set_color(143/255, 143/255, 188/255)
+            print "\t&" + str(stack[i])
+            if pointer_to_name(stack[i]) != "":
+                console.set_color(0, 0, 0)
+                print "\t\tpointer to: " + pointer_to_name(stack[i])
         elif types[i] == TYPE_FLAG:
+            console.set_color(142/255, 35/255, 107/255)
             if stack[i] == -1:
-                sys.stdout.write("true")
+                print "true"
             elif stack[i] == 0:
-                sys.stdout.write("false")
+                print "false"
             else:
-                sys.stdout.write("malformed flag")
+                print "malformed flag"
         else:
-            sys.stdout.write("unmatched type on stack!")
-        sys.stdout.write("\n")
+            print "unmatched type on stack!"
         i += 1
 
 
@@ -1391,28 +1401,43 @@ def dump_dict():
     l = ''
     for w in dictionary_names:
         l = l + w + ' '
-    sys.stdout.write(l)
-    sys.stdout.write("\n")
+    print l
 
 
 
-if __name__ == '__main__':
+def interface():
     global bootstrap
-    console.clear()
-    sys.stdout.write("parable for pythonista\n\n")
+
     prepare_slices()
     prepare_dictionary()
     parse_bootstrap(bootstrap)
     collect_unused_slices()
 
+    console.clear()
+
     completed = 0
     counter = 0
 
     while completed == 0:
-        console.set_color(.16, .128, .112)
-        sys.stdout.write("\n>> ")
-        sys.stdout.flush()
+
+        console.clear()
+
+        dump_stack()
+
         console.set_color(0, 0, 0)
+
+        print '- - - - - - - - - -'
+
+        for e in errors:
+            console.set_color(214/255, 111/255, 98/255)
+            print e
+
+        if (len(errors) > 0):
+            print '- - - - - - - - - -'
+
+        clear_errors()
+
+        sys.stdout.flush()
 
         src = sys.stdin.readline()
 
@@ -1420,16 +1445,9 @@ if __name__ == '__main__':
             completed = 1
         elif ' '.join(src.split()) == ':defined':
             dump_dict()
-        elif ' '.join(src.split()) == ':stack':
-            dump_stack()
         else:
-            if len(src) > 1:
+            if len(src) > 0:
                 interpret(compile(src, request_slice()))
-
-        for e in errors:
-            sys.stdout.write(e)
-
-        clear_errors()
 
         counter += 1
         if counter > 100:
@@ -1437,5 +1455,9 @@ if __name__ == '__main__':
                 collect_unused_slices()
                 counter = 0
 
-        console.set_color(0, 0, 0)
-        sys.stdout.flush()
+
+#
+# begin execution
+#
+
+interface()
