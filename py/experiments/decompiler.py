28794b4b3007257d72996686f32041ec30bea025
Charles Childers <crc@forthworks.com>
2015-11-30 11:34:10 +0000

    begin work on updating the decompiler code

--- py/experiments/decompiler.py
+++ py/experiments/decompiler.py
@@ -1,56 +1,45 @@
 # parable byte code decompiler
-# Copyright (c) 2014, Charles Childers
+# Copyright (c) 2014-2015, Charles Childers
 
-from parable import *
+import parable
+import sys
+
 
 def pointer_to_name(ptr):
-    global dictionary_names, dictionary_slices
     s = ""
-    if ptr in dictionary_slices:
-        s = dictionary_names[dictionary_slices.index(ptr)]
+    if ptr in parable.dictionary_slices:
+        s = parable.dictionary_names[parable.dictionary_slices.index(ptr)]
     return s
 
 
 def deconstruct(slice):
-    global SLICE_LEN
     i = 0
     s = '[ '
-    while i < (SLICE_LEN - 1):
-        o = fetch(slice, i)
-        if o == BC_PUSH_N:
-            i += 1
-            o = fetch(slice, i)
+    while i <= parable.get_last_index(slice):
+        o = parable.fetch(slice, i)
+        t = parable.fetch_type(slice, i)
+        if t == parable.TYPE_NUMBER:
             s += '#' + str(o)
-        elif o == BC_PUSH_C:
-            i += 1
-            o = fetch(slice, i)
+        elif t == parable.TYPE_BYTECODE:
+            s += '`' + str(o)
+        elif t == parable.TYPE_CHARACTER:
             s += '$' + str(chr(o))
-        elif o == BC_PUSH_S:
-            i += 1
-            o = fetch(slice, i)
-            s += "'" + slice_to_string(o) + "'"
-        elif o == BC_PUSH_COMMENT:
-            i += 1
-            o = fetch(slice, i)
-            s += '"' + slice_to_string(o) + '"'
-        elif o == BC_PUSH_F:
-            i += 1
-            o = fetch(slice, i)
+        elif t == parable.TYPE_STRING:
+            s += "'" + parable.slice_to_string(o) + "'"
+        elif t == parable.TYPE_COMMENT:
+            s += "'\"" + parable.slice_to_string(o) + "\""
+        elif t == parable.TYPE_POINTER:
             x = pointer_to_name(o)
             if len(x) == 0:
                 s += deconstruct(o)
             else:
                 s += '&' + x
-        elif o == BC_FLOW_CALL:
-            i += 1
-            o = fetch(slice, i)
+        elif t == parable.TYPE_FUNCTION_CALL:
             x = pointer_to_name(o)
             if len(x) == 0:
                 s += '&' + str(o) + ' invoke'
             else:
                 s += x
-        elif o == BC_FLOW_RETURN:
-            i = SLICE_LEN
         else:
             s += '`' + str(o)
         s += ' '
@@ -62,32 +51,38 @@ def deconstruct(slice):
 def trace(s):
     deps = set()
     deps.add(s)
-    for x in find_references(s):
+    for x in parable.find_references(s):
         deps.add(x)
-        for i in find_references(x):
+        for i in parable.find_references(x):
             deps.add(i)
     return deps
 
 
 def generate_source(x):
-    global dictionary_names, dictionary_slices
     src = ""
     for s in sorted(trace(x)):
         if pointer_to_name(s) != 'define':
             if pointer_to_name(s) != '':
-                src += "[ ] '" + pointer_to_name(s) + "' define\n"
                 src += deconstruct(s)
                 src += " '" + pointer_to_name(s)
                 src += "' define\n"
     return src + "\n"
 
+
 def generate_full_source():
-    global dictionary_names, dictionary_slices
     src = ""
-    for s in dictionary_slices:
+    for s in parable.dictionary_slices:
         if pointer_to_name(s) != 'define':
-            src += "[ ] '" + pointer_to_name(s) + "' define\n"
             src += deconstruct(s)
             src += " '" + pointer_to_name(s)
             src += "' define\n"
     return src + "\n"
+
+
+if __name__ == '__main__':
+    parable.prepare_slices()
+    parable.prepare_dictionary()
+    parable.parse_bootstrap(open('stdlib.p').readlines())
+    parable.collect_garbage()
+
+    print generate_full_source().encode('utf-8')
