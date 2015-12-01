bb9cbd4298173898969a86f85da506c73f461f71
Charles Childers <crc@forthworks.com>
2015-12-01 11:20:13 +0000

    fix a bug in pre

--- py/convert.py
+++ py/convert.py
@@ -4,12 +4,32 @@
 print "#!/usr/bin/env python"
 print "# Copyright (c)2012-2013, Charles Childers"
 
+def condense_lines(code):
+    m = len(code)
+    s = ''
+    r = []
+    i = 0
+    c = 0
+    while i < m:
+        if code[i].endswith(' \\\n'):
+            s = s + ' ' + code[i][:-2].strip()
+            c = 1
+        else:
+            c = 0
+            s = s + ' ' + code[i]
+        if c == 0:
+            if s != '' and s != ' \n':
+                r.append(s.strip())
+            s = ''
+        i = i + 1
+    return r
+
 f = open('parable.py').readlines()
 for line in f:
     print line.rstrip()
 
 print "bootstrap = []"
-f = open('stdlib.p').readlines()
+f = condense_lines(open('stdlib.p').readlines())
 for line in f:
     if len(line) > 1:
         print 'bootstrap.append(""" ' + line.strip() + ' """)'
