f3936452ab79729121a4ae5537291d1a0dc7bab8
Charles Childers <crc@forthworks.com>
2016-03-08 03:14:59 +0000

    some work on gen-snapshot.p (prep for bringing in optional stripping of unused functions

--- py/gen-snapshot.py
+++ py/gen-snapshot.py
@@ -8,18 +8,23 @@ import parable
 import os
 import sys
 
-if __name__ == '__main__':
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+def prepare():
     parable.prepare_slices()
     parable.prepare_dictionary()
     parable.parse_bootstrap(open('stdlib.p').readlines())
 
+
+def load_files():
     if len(sys.argv) > 1:
         for i in sys.argv:
             if os.path.exists(i) and i != "./gen-snapshot.py" and i != "gen-snapshot.py" :
                 parable.parse_bootstrap(open(i).readlines())
 
-    parable.collect_garbage()
 
+def create_snapshot():
+    parable.collect_garbage()
     j = json.dumps({"symbols": parable.dictionary_names, \
                     "symbol_map": parable.dictionary_slices, \
                     "errors": parable.errors, \
@@ -30,11 +35,27 @@ if __name__ == '__main__':
                     "memory_map": parable.memory_map, \
                     "memory_sizes": parable.memory_size, \
                     "hidden_slices": parable.dictionary_hidden_slices, })
+    return j
 
+
+def compress_snapshot(j):
     try:
         c = bz2.compress(bytes(j, 'utf-8'))
     except:
         c = bz2.compress(j)
+    return c
 
+
+def encode_snapshot(c):
+    return str(base64.b64encode(c)).replace("b'", "").replace("'", "")
+
+# -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+if __name__ == '__main__':
+    prepare()
+    load_files()
+    j = create_snapshot()
+    c = compress_snapshot(j)
+    e = encode_snapshot(c)
     with open('parable.snapshot', 'w') as file:
-        file.write(str(base64.b64encode(c)).replace("b'", "").replace("'", ""))
+        file.write(e)
