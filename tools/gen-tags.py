7ff1e928b1e2db49d5d48a5618f090a7e3d3ad90
Charles Childers <crc@forthworks.com>
2016-04-02 21:09:41 -0400

    gen-tags now generate tags for vim and tmtags for textmate

--- tools/gen-tags.py
+++ tools/gen-tags.py
@@ -7,17 +7,23 @@
 import fnmatch
 import os
 
-def tag_for_colon(l, f, i):
+def tag(tag, file, line, textmate):
+    if textmate:
+        return tag + '\t' + os.getcwd() + '/' + file[2:] + '\t' + '/^\'' + tag + '\'$/;"\tfunction\tline:' + str(line)
+    else:
+        return tag + '\t' + os.getcwd() + '/' + file[2:] + '\t' + str(line)
+
+def tag_for_colon(l, f, i, textmate):
     t = l.split(' ')
     print('  ' + t[-2:-1][0][1:-1])
-    return t[-2:-1][0][1:-1] + '\t' + f + '\t' + str(i)
+    return tag(t[-2:-1][0][1:-1], f, i, textmate)
 
-def tag_for_dot(l, f, i):
+def tag_for_dot(l, f, i, textmate):
      t = l.strip().split(' ')
      print('  ' + t[0:1][0][1:-1])
-     return t[0:1][0][1:-1] + '\t' + f + '\t' + str(i)
+     return tag(t[0:1][0][1:-1], f, i, textmate)
 
-def get_tags_for(pat):
+def get_tags_for(pat, textmate=False):
     tags = []
     matches = []
     for root, dir, filenames in os.walk('.'):
@@ -28,16 +34,21 @@ def get_tags_for(pat):
         s = open(f, 'r').readlines()
         i = 1
         for l in s:
-            if l.endswith('\' :\n'): tags.append(tag_for_colon(l, f, i))
-            if l.strip().startswith('\'') and l.endswith(' .\n'): tags.append(tag_for_dot(l, f, i))
+            if l.endswith('\' :\n'): tags.append(tag_for_colon(l, f, i, textmate))
+            if l.strip().startswith('\'') and l.endswith(' .\n'): tags.append(tag_for_dot(l, f, i, textmate))
             i = i + 1
     return tags
 
 if __name__ == '__main__':
     with open('tags', 'w') as f:
-#        f.write('!_TAG_FILE_FORMAT 1\n!_TAG_FILE_SORTED 1\n')
         tags = get_tags_for('*.p')
         tags = tags + get_tags_for('*.md')
         for l in sorted(tags):
             f.write(l + '\n')
         print(str(len(tags)) + ' words found')
+
+    with open('tmtags', 'w') as f:
+        tags = get_tags_for('*.p', textmate=True)
+        tags = tags + get_tags_for('*.md', textmate=True)
+        for l in sorted(tags):
+            f.write(l + '\n')
